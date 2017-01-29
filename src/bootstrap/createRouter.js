b4d54d40b2eab36abbd3b5cf228c1fc4fe2a3787
Max Eddy <maxiscool@gmail.com>
2017-01-28 23:02:02 -0500

    refactor route guards

--- src/bootstrap/createRouter.js
+++ src/bootstrap/createRouter.js
@@ -1,64 +1,23 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {browserHistory, IndexRoute, Route, Router} from 'react-router';
-import {requestSignIn, requestWorkoutTemplates} from '../actions/creators';
 import {NoMatch, App, Exercises, Home, Workouts, WorkoutTemplate, WorkoutTemplateEdit} from '../containers';
-import {composeChecks, createGuard, createStateCheck} from '../helpers/guardHelpers';
-import {getAuthReceived, getAuthenticated, getWorkoutTemplatesReceived, getWorkoutTemplate} from '../helpers/selectors';
+import createRouteGuards from '../helpers/createRouteGuards';
 
 export default function createRouter(store) {
-  // Need to potentially rework this because you can put guards on a parent route.
-  const isAuthenticated = createStateCheck(
-    requestSignIn,
-    getAuthReceived,
-    getAuthenticated
-  );
-
-  const workoutTemplateExists = composeChecks([
-    isAuthenticated,
-    createStateCheck(
-      requestWorkoutTemplates,
-      getWorkoutTemplatesReceived,
-      getWorkoutTemplate
-    )
-  ]);
-
-  const guardAuth = createGuard(
-    isAuthenticated,
-    '/',
-    store
-  );
-
-  const guardWorkoutTemplate = createGuard(
-    workoutTemplateExists,
-    '/no-match',
-    store
-  );
-
+  const routeGuards = createRouteGuards(store);
   return <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={Home} />
-        <Route
-          path="workouts"
-          component={Workouts}
-          onEnter={guardAuth}
-        />
-        <Route
-          path="exercises"
-          component={Exercises}
-          onEnter={guardAuth}
-        />
-        <Route
-          path="workout-template/:workoutTemplateKey"
-          component={WorkoutTemplate}
-          onEnter={guardWorkoutTemplate}
-        />
-        <Route
-          path="workout-template/:workoutTemplateKey/edit"
-          component={WorkoutTemplateEdit}
-          onEnter={guardWorkoutTemplate}
-        />
+        <Route onEnter={routeGuards.auth}>
+          <Route path="workouts" component={Workouts} />
+          <Route path="exercises" component={Exercises} />
+          <Route path="workout-template" onEnter={routeGuards.workoutTemplate}>
+            <Route path=":workoutTemplateKey" component={WorkoutTemplate} />
+            <Route path=":workoutTemplateKey/edit" component={WorkoutTemplateEdit} />
+          </Route>
+        </Route>
         <Route path="*" component={NoMatch} />
       </Route>
     </Router>
---
+++ src/helpers/createRouteGuards.js
@@ -0,0 +1,55 @@
+import {requestSignIn, requestWorkoutTemplates} from '../actions/creators';
+import {getAuthReceived, getAuthenticated, getWorkoutTemplatesReceived, getWorkoutTemplate} from '../helpers/selectors';
+
+export default function createRouteGuards(store) {
+  const isAuthenticated = createStateCheck(
+    requestSignIn,
+    getAuthReceived,
+    getAuthenticated
+  );
+
+  const workoutTemplateExists = createStateCheck(
+    requestWorkoutTemplates,
+    getWorkoutTemplatesReceived,
+    getWorkoutTemplate
+  );
+
+  return {
+    auth: createGuard(isAuthenticated, '/'),
+    workoutTemplate: createGuard(workoutTemplateExists, '/no-match')
+  };
+
+  function createGuard(check, failRoutePath) {
+    return (newRouteState, replace, cb) => {
+      check(store, newRouteState).then(result => {
+        if (!result) {
+          replace(failRoutePath);
+        }
+
+        cb();
+      });
+    };
+  }
+};
+
+export function createStateCheck(createRequestAction, selectReady, selectCheck) {
+  return (store, routeState) => {
+    return new Promise(resolve => {
+      const storeState = store.getState();
+      if (selectReady(storeState, routeState)) {
+        resolve(selectCheck(storeState, routeState));
+        return;
+      }
+
+      store.dispatch(createRequestAction());
+
+      const unsubscribe = store.subscribe(() => {
+        const storeState = store.getState();
+        if (selectReady(storeState, routeState)) {
+          unsubscribe();
+          resolve(selectCheck(storeState, routeState));
+        }
+      });
+    });
+  };
+}
